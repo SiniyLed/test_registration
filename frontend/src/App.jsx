ebc878b4947110c9c96e724eb0f87cdca7b34c83
Aleksej1024 <aleksejpropastin@gmail.com>
2023-12-19 23:39:33 +0300

    Fix Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './components/Header.jsx'
+import Header from './components/Header.jsx'
 function App() {
   
 
   return (
-    <>
+    <div>
         <Header pageTitle="Privet"/>
-    </>
+    </div>
   )
 }
 
