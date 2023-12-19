1f9353e6b5a41bcf245934082b3d48235d419463
alinka <mankovskaya.a.p@edu.mirea.ru>
2023-12-19 23:12:51 +0300

    Add header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './components/Header.jsx'
+function App() {
+  
+
+  return (
+    <>
+        <Header pageTitle="Privet"/>
+    </>
+  )
+}
+
+export default App
